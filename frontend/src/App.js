2d51b4a8f1032b045fc540f5b84a474855195ba9
s1Develop <sungwon.dev@gmail.com>
2023-09-10 14:18:32 -0700

    implemented navbar s1dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,16 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './components/navbar/Layout.js';
-import Home from "./components/navbar/Home.js";
-import Contact from './components/navbar/Contact.js';
-import AboutUs from "./components/navbar/AboutUs.js";
+import { Navbar } from './components'
+//  from "./components/navbar/Navbar"
 import "./App.css";
 
 
 function App() {
   return (
-    <div className="home">
-      <div className="navbar">
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path="contact" element={<Contact />} />
-                <Route path="*" element={<AboutUs />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
       </div>
+
     </div>
   );
 }
